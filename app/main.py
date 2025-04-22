a0d56069d1f975121f9ee44d88677583d94b9a78
dibkb <dibas9110@gmail.com>
2025-04-23 00:05:41 +0530

    chaching

--- app/main.py
+++ app/main.py
@@ -1,7 +1,10 @@
+import json
 from fastapi import FastAPI
 from .youtube_api import YoutubeAPI
 from .llm import LanguageModel
 from .settings import config
+import redis
+import hashlib
 
 app = FastAPI()
 
@@ -9,24 +12,33 @@ app = FastAPI()
 # singleton initialization
 YoutubeAPI()
 LanguageModel()
+redis_client = redis.Redis(host="redis", port=6379, db=0)
+
 
 @app.get("/")
 async def root():
     return "Comment sentiment v1"
 
 
-
 @app.post("/get-comments/{video_id}")
-async def get_comments(video_id: str):
+async def get_comments(video_id: str, next_page_token: str = None):
     youtube_client = YoutubeAPI().get_youtube_client()
+
+    token = next_page_token or ""
+    hash_key = hashlib.sha256((video_id + token).encode('utf-8')).hexdigest()
+
+    cached_response = redis_client.get(hash_key)
+    if cached_response:
+        return json.loads(cached_response)
 
     request = youtube_client.commentThreads().list(
         part="snippet",
         videoId=video_id,
         maxResults=config.MAX_RESULTS,
         textFormat="plainText",
-        order="relevance"
+        order="relevance",
+        pageToken=next_page_token,
     )
     response = request.execute()
-
+    redis_client.setex(hash_key, 3600, json.dumps(response))
     return response
